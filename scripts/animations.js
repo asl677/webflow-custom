27a530b41ed04257616ab9d885fe68b1a38a1bc6
Alex <alex.lakas@gmail.com>
2025-04-27 09:22:12 -0700

    Add: Include flex-badge class in GSAP animations

--- scripts/animations.js
+++ scripts/animations.js
@@ -48,7 +48,7 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 
   // Select all text elements we want to animate (excluding navigation)
-  const textElements = document.querySelectorAll('.sticky-wrap h1, .sticky-wrap h2, .sticky-wrap h3, .sticky-wrap h4, .sticky-wrap h5, .sticky-wrap h6, .sticky-wrap p, .sticky-wrap .heading, .btn-show');
+  const textElements = document.querySelectorAll('.flex-badge, .sticky-wrap h1, .sticky-wrap h2, .sticky-wrap h3, .sticky-wrap h4, .sticky-wrap h5, .sticky-wrap h6, .sticky-wrap p, .sticky-wrap .heading, .btn-show');
 
   // Create timeline for each text element
   textElements.forEach((element, index) => {
